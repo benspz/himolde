5653ab48a81badbf825dba43c34ccf26df79925c
benspz <ben.espeseth@proton.me>
2024-09-22 23:16:01 +0200

    preclean

--- oblig3.py
+++ oblig3.py
@@ -1,7 +1,7 @@
-#IBE152 H24 Oblig 3
-#Skrevet av Benjamin Espeseth
+# IBE152 H24 Oblig 3
+# Skrevet av Benjamin Espeseth
 
-#---------Deloppgave 1--------------------------------------------------------
+# ---------Deloppgave 1--------------------------------------------------------
 def erTall(a, b, c):
     return isinstance(a, (int, float)) and isinstance(b, (int, float)) and isinstance(c, (int, float))
 
@@ -15,18 +15,29 @@ def innafor(x, fra, til):
     else:
         return False
 
-x=50
+
+x = 50
 print("Test av innafor()")
-print(innafor(x,40,80))         # skal gi True i dette eksempelet
-print(innafor(x,70,30))         # skal gi True i dette eksempelet
-print(innafor(x,x,x))           # skal gi True uansett verdi av x
-print(innafor(x, x-0.1, x-0.2))   # skal gi False uansett verdi av x
-print(innafor(x, 2*x, 0.5*x))     # skal gi True uansett verdi av x
+print(innafor(x, 40, 80))  # skal gi True i dette eksempelet
+print(innafor(x, 70, 30))  # skal gi True i dette eksempelet
+print(innafor(x, x, x))  # skal gi True uansett verdi av x
+print(innafor(x, x - 0.1, x - 0.2))  # skal gi False uansett verdi av x
+print(innafor(x, 2 * x, 0.5 * x))  # skal gi True uansett verdi av x
 print(innafor("kake", 20, 70))
 
 
+# ---------Deloppgave 2--------------------------------------------------------
 
-#---------Deloppgave 2--------------------------------------------------------
+def countdown(n):
+    if n == 0:
+        print("Blastoff!")
+    elif n > 0:
+        print(n)
+        countdown(n - 1)
+    else:
+        print(n)
+        countdown(n + 1)
+
 
 def countdownPar(n):
     if n > 0:
@@ -35,18 +46,18 @@ def countdownPar(n):
         else:
             if n % 2 == 0:
                 print(n)
-                countdownPar(n-1)
+                countdownPar(n - 1)
             else:
-                countdownPar(n-1)
+                countdownPar(n - 1)
     else:
         if n == 0:
             print("Blastoff!")
         else:
             if n % 2 == 0:
                 print(n)
-                countdownPar(n+1)
+                countdownPar(n + 1)
             else:
-                countdownPar(n+1)
+                countdownPar(n + 1)
 
 
 def countdownOdde(n):
@@ -56,18 +67,18 @@ def countdownOdde(n):
         else:
             if n % 2 != 0:
                 print(n)
-                countdownOdde(n-1)
+                countdownOdde(n - 1)
             else:
-                countdownOdde(n-1)
+                countdownOdde(n - 1)
     else:
         if n == 0:
             print("Blastoff!")
         else:
             if n % 2 != 0:
                 print(n)
-                countdownOdde(n+1)
+                countdownOdde(n + 1)
             else:
-                countdownOdde(n+1)
+                countdownOdde(n + 1)
 
 
 def countdown2(n, parFlagg):
@@ -78,18 +89,18 @@ def countdown2(n, parFlagg):
             else:
                 if n % 2 == 0:
                     print(n)
-                    countdown2(n-1, True)
+                    countdown2(n - 1, True)
                 else:
-                    countdown2(n-1, True)
+                    countdown2(n - 1, True)
         else:
             if n == 0:
                 print("Blastoff!")
             else:
                 if n % 2 != 0:
                     print(n)
-                    countdown2(n-1, False)
+                    countdown2(n - 1, False)
                 else:
-                    countdown2(n-1, False)
+                    countdown2(n - 1, False)
     else:
         if parFlagg:
             if n == 0:
@@ -97,27 +108,44 @@ def countdown2(n, parFlagg):
             else:
                 if n % 2 == 0:
                     print(n)
-                    countdown2(n+1, True)
+                    countdown2(n + 1, True)
                 else:
-                    countdown2(n+1, True)
+                    countdown2(n + 1, True)
         else:
             if n == 0:
                 print("Blastoff!")
             else:
                 if n % 2 != 0:
                     print(n)
-                    countdown2(n+1, False)
+                    countdown2(n + 1, False)
                 else:
-                    countdown2(n+1, False)
+                    countdown2(n + 1, False)
 
+
+print("\nTest av countdown(6)")
+countdown(6)
 print("\nTest av countdownOdde(6)")
 countdownOdde(6)
 print("\nTest av countdownPar(6)")
 countdownPar(6)
 print("\nTest av countdown2(10, True)")
-countdown2(10, True)
+countdown2(6, True)
 print("\nTest av countdown2(3, False)")
-countdown2(3, False)
+countdown2(6, False)
 
 
-#---------Deloppgave 3--------------------------------------------------------
+# ---------Deloppgave 3--------------------------------------------------------
+
+def boksInnenfor(a, b, c, x, y, z):
+    measurements = [a, b, c]  # https://www.w3schools.com/python/python_lists_sort.asp
+    measurements.sort(reverse=True)
+    if measurements[0] <= x and measurements[1] <= y and measurements[2] <= z:
+        return True
+    else:
+        return False
+
+
+print("\nTest av boksInnenfor(90, 20, 50, 100, 30, 20)")
+print(boksInnenfor(90, 20, 50, 100, 30, 20))  # This should be False
+print("\nTest av boksInnenfor(10, 20, 30, 30, 20, 10)")
+print(boksInnenfor(10, 20, 30, 30, 20, 10))  # This should be True
