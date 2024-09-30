2bc7aca9cc1b1b1a59d3e4b4380c9c2827d599ab
benspz <ben.espeseth@proton.me>
2024-09-30 18:36:06 +0200

    comments

---
+++ oblig4temp.py
@@ -0,0 +1,19 @@
+import os
+
+#filer = os.listdir()
+filer = [f"Item {i+1}" for i in range(50)] #testlist
+
+antall = 0
+nytt_antall = 0
+while  antall < len(filer):
+    print(filer[antall])
+    antall += 1
+
+    if antall == nytt_antall + 20:
+        nytt_antall = antall
+        svar = input(f"\nHar vist {antall} filer av {len(filer)}, vil du fortsette? (y/n) >> ")
+        if svar != "y".lower():
+            break
+    elif antall >= len(filer):
+        print(f"\nHar vist alle {len(filer)} filer")
+        break
